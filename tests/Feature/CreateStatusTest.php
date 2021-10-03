6c961ddaf8914b0bfbd8c1527777f554b0551aba
EduMogro <edumogro@outlook.com>
2021-10-03 03:08:03 -0300

    test: verify that the body is not empty and has a minimum of characters

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -23,12 +23,12 @@ class CreateStatusTest extends TestCase
     /** @test */
     public function an_authenticated_user_can_create_a_statuses()
     {
-        $this->withoutExceptionHandling();
+        // $this->withoutExceptionHandling();
 
         $user = User::factory()->create();
         $this->actingAs($user);
 
-        $response = $this->post(route('statuses.store'), [
+        $response = $this->postJson(route('statuses.store'), [
             'user_id' => $user->id,
             'body' => 'Mi primer estado'
         ]);
@@ -39,5 +39,40 @@ class CreateStatusTest extends TestCase
         ]);
         
     }
+
+    /** @test */
+    public function a_status_requires_a_body()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), [
+            'body' => ''
+        ]);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+
+    /** @test */
+    public function a_status_body_requires_a_minimum_length()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), [
+            'body' => '1234'
+        ]);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+    
     
 }
