cb9a32baf698c5518107f038458e527a1cd76ed8
EduMogro <edumogro@outlook.com>
2021-11-08 03:31:23 -0300

    feat: create a status component and its test

---
+++ resources/js/components/StatusForm.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <form @submit.prevent="submit">
+            <div class="card-body">
+                <textarea v-model="body" name="body" class="form-control border-0" placeholder="¿En qué estás pensando?"></textarea>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary" id="create-status">Publicar</button>
+            </div>
+        </form>
+        <div v-for="(status, index) in statuses" :key="index">
+            {{ status.body }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            body: '',
+            statuses: []
+        }
+    },
+    methods: {
+        submit() {
+            axios.post('/statuses', {body: this.body})
+                 .then(res => {
+                    //  console.log(res.data)
+                    this.statuses.push(res.data);
+                    this.body = ''
+                 })
+                 .catch(err => {
+                     console.log(err.response.data)
+                 })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
